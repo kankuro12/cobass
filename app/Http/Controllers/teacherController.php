9c4919e8a40de642fad61916c5aec803a4228281
[Saansarbaba] <[sansaarbaba@gmail.com]>
2025-03-08 16:54:34 +0545

    Fix class naming conventions for Teacher and Testimonial models and controllers

--- app/Http/Controllers/teacherController.php
+++ app/Http/Controllers/teacherController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\teacher;
+use App\Models\Teacher;
 use Illuminate\Support\Facades\DB;
 
 class TeacherController extends Controller
