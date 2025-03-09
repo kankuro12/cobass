4d1db7c43e329c818533a6d3858908e09aab1815
chhatraman shrestha <cms111000111@gmail.com>
2025-03-09 13:40:45 +0545

    Refactor course index view: update button labels, enhance course name display with heading and separator

--- resources/views/admin/course/index.blade.php
+++ resources/views/admin/course/index.blade.php
@@ -1,21 +1,16 @@
-
 @extends('admin.layout.app')
 @section('s-title')
     <li class="breadcrumb-item active">
         Course
     </li>
 @endsection
-
 @section('css')
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('admin/plugins/drophify/css/dropify.min.css') }}">
 @endsection
-
 @section('s-title')
 @endsection
 @section('page-option')
     <div class="text-right">
-       <a href="{{ route('admin.course.add') }}" class="btn btn-primary">Course Add </a>
+        <a href="{{ route('admin.course.add') }}" class="btn btn-primary">Add Course</a>
     </div>
 @endsection
 
@@ -23,39 +18,33 @@
     <div class="bg-white p-3">
         <div class="row">
             @foreach ($courses as $course)
-            <div class="col-md-4 mb-3">
-                <div class="shadow">
-                    <div class="d-flex" style="height: 150px;align-items: center;overflow: hidden;background: gray; ">
+                <div class="col-md-4 mb-3">
+                    <div class="shadow">
+                        <div class="d-flex" style="height: 150px;align-items: center;overflow: hidden;background: gray; ">
 
-                        <img src="{{ asset($course->image) }}" alt="" class="w-100">
+                            <img src="{{ asset($course->image) }}" alt="" class="w-100">
+                        </div>
+                        <div class="p-3">
+                            <h4>
+                                {{ $course->name }}
+                            </h4>
+                            <hr>
+                            <div class="d-flex justify-content-between">
+
+                                <a href="{{ route('admin.course.edit', ['course' => $course->id]) }}"
+                                    class="btn btn-primary">Edit</a>
+
+                                <a href="{{ route('admin.course.del', ['course' => $course->id]) }}"
+                                    class="btn btn-danger">Delete</a>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="p-3">
-                            {{ $course->name }}
-                            <div class="d-flex justify-content-between" >
-
-                        <a href="{{ route('admin.course.edit',['course' => $course->id])}}"
-                         class="btn btn-primary">Edit</a>
-
-                        <a href="{{ route('admin.course.del',['course'=> $course->id])}}"
-                         class="btn btn-danger">Delete</a>
-                    </div>
-                    </div>
-
                 </div>
-            </div>
-
             @endforeach
 
         </div>
     </div>
 @endsection
 @section('script')
-    <script src="{{ asset('admin/plugins/drophify/js/dropify.min.js') }}"></script>
-    <script>
-        $(function() {
-            $('.photo').dropify();
-            $('#desc').summernote();
-
-        });
-    </script>
 @endsection
